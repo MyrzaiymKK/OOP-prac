b1ea986d45eb9d0ecb0f172d183913a0be5e57cd
Myrzaiym <myrzaiymkeldibekova1@gmail.com>
2023-11-03 17:54:12 +0600

    prac

---
+++ src/Passport.java
@@ -0,0 +1,23 @@
+public class Passport {
+    int id;
+    String name;
+    String LastName;
+    int birth;
+    String gender;
+    String country;
+
+    public Passport(int id,String name,String LastName,int birth,String gender,String country){
+        this.id = id;
+        this.name = name;
+        this.LastName = LastName;
+        this.birth = birth;
+        this.gender = gender;
+        this.country = country;
+    }
+    public Passport(){
+
+    }
+
+
+    }
+
---
+++ src/Students.java
@@ -0,0 +1,14 @@
+public class Students {
+    String name;
+    int age;
+    String group;
+
+    public Students(String name,int age,String group){
+       this.name = name;
+       this.age = age;
+       this.group = group;
+    }
+    public Students(){
+
+    }
+}
